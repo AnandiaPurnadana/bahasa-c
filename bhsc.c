d3c82c674dc398747fb8bcc65ec711d63ef943d2
AnandiaPurnadana <116157823+AnandiaPurnadana@users.noreply.github.com>
2022-11-15 11:15:36 +0700

    Add files via upload

---
+++ bhsc.c
@@ -0,0 +1,9 @@
+
+#include<stdio.h>
+int main()
+{
+    printf("\n");
+    printf("Nama              : Anandia \n");
+    printf("Jenis Kelamin     : Wanita\n");
+    printf("Alamat            : Bali\n");
+}
